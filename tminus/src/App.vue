7245c76e40a85df415397cef19907d429ddd0059
seglem <carolynseglem@gmail.com>
2022-09-11 18:51:01 -0500

    Initial project creation.
    Created main app page with top toolbar and right-side navbar.
    Created CalendarBase component, which currently contains only a calendar.

---
+++ tminus/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#c53131"
+      dark
+      clipped-right    
+    >
+    <v-app-bar-title>
+      T-Minus
+    </v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      absolute
+      permanent
+      right
+      app
+      clipped
+    >
+    <v-list>
+      <v-list-item>
+        MY ACCOUNT
+      </v-list-item>
+      <v-list-item>
+        Home
+      </v-list-item>
+      <v-list-item>
+        Settings
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+    <v-main>
+      <CalendarBase/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import CalendarBase from './components/CalendarBase';
+
+export default {
+  name: 'App',
+
+  components: {
+    CalendarBase,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ tminus/src/components/CalendarBase.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container>
+        <v-calendar/>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'CalendarBase'
+}
+  </script>
