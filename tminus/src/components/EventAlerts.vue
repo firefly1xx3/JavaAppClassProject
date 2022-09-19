d5fffc37366c9a679f17be15d3c63371d02d48a6
viktorcus <carolynseglem@gmail.com>
2022-09-19 00:39:08 -0500

    Added alerts section to the top of dashboard to show today's events.
    Added json file with sample events as temporary stand-in data.
    Some adjustments to NavBar.

---
+++ tminus/src/components/EventAlerts.vue
@@ -0,0 +1,22 @@
+<template>
+    <div style="padding:1rem; width:100%" >
+        <div v-for="event in events" :key="event"> 
+            <v-alert 
+                :color=event.color 
+                text
+                dense
+            >
+                {{`${new Date(event.start).toLocaleString()}: ${event.name}`}}
+            </v-alert>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EventAlerts", 
+        props: {
+            events: []
+      },
+    }
+</script>
---
+++ tminus/src/components/NavDrawer.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-navigation-drawer
+        absolute
+        permanent
+        left
+        app
+        clipped
+        >
+        <v-list dense nav>
+            <v-list-item>
+                <v-list-item-content>MY ACCOUNT</v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+                <v-list-item-content>
+                    <router-link 
+                        to="/"
+                        style="text-decoration: none; color: inherit;"
+                    >
+                        Home
+                    </router-link>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+                <v-list-item-content>
+                    <router-link 
+                        to="/settings"
+                        style="text-decoration: none; color: inherit;"
+                    >
+                        Settings
+                    </router-link>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    export default {
+        name: "NavDrawer"
+    }
+</script>
